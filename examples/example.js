a5903b2e32570f01c669d507537f74e5092dff6f
Nathan Abraham <nabraham@somedomain.com>
2014-10-01 20:37:30 -0400

    first cut

---
+++ examples/example.js
@@ -0,0 +1,33 @@
+var bar = require('../index');
+
+var s1 = [];
+var s2 = [];
+var s3 = [];
+var pi = 3.1415962535;
+var N = 50;
+for (var i=0; i<50; i++) {
+    s1.push(0.5 + 0.5*Math.cos(i/(N-1) * 6*pi));
+    s2.push(0.5 + 0.5*Math.sin(i/(N-1) * 6*pi));
+    s3.push(0.5 + 0.5*Math.sin(i/(N-1) * 3*pi));
+}
+
+var series = [s1, s2, s3];
+console.log(bar(series, {title: 'Trig Waves'}));
+
+series = [[1,2,3],[1,1,1]];
+console.log(bar(series, {height: 9, width: 30, icon: '##'}));
+
+var failures = [
+    (function () { bar(123, {}) }),
+    (function () { bar([], {}) }),
+    (function () { bar([[1,2,3],[1]], {}) }),
+    (function () { bar([[1,2,3],[1,2,'three']], {}) })
+]
+
+failures.forEach(function(f) {
+    try {
+        f();
+    } catch (e) {
+        console.log(e);
+    }
+});
---
+++ index.js
@@ -0,0 +1,88 @@
+var colors = require('colors')
+  , _ = require('lodash');
+
+module.exports = function(series, options) {
+
+    if (!Array.isArray(series)) {
+        throw new Error('series must be an array of numbers or an array of equal length arrays of numbers');
+    }
+
+    if (series.length === 0) {
+        throw new Error('Series must be of length > 0');
+    }
+
+    if (!Array.isArray(series[0])) {
+        series = [series];
+    }
+
+    var lengths = _.map(series, _.size);
+    if (_.uniq(lengths).length !== 1) {
+        throw new Error('Series must all be equal length: ' + lengths.join(','));
+    }
+
+    if (!_.every(_.map(series, function(s) {
+        var types = _.map(s, function(i) { return typeof i; });
+        var utypes = _.uniq(types);
+        return utypes.length === 1 && utypes[0] === 'number';
+    }))) {
+        throw new Error('All series must have only numbers');
+    }
+
+    options = options ? options : {};
+    options.icon = options.icon? options.icon : 'xo+*^@';
+    options.color = options.color? options.color : ['red', 'blue', 'green', 'yellow', 'cyan'];
+    options.height = options.height ? options.height : 24;
+
+    var m = createMatrix(series, options);
+    return drawMatrix(m, series, options);
+};
+
+function createMatrix(series, options) {
+    var m = {};
+    var max = _.max(_.map(series, _.max));
+
+    for (var s = 0; s < series.length; s++) {
+        var serum = series[s];
+        var marker = {};
+        marker.icon = options.icon[s % options.icon.length];
+        marker.color = options.color[s % options.color.length];
+        for (var i=0; i < serum.length; i++) {
+            for (var j=0; j < serum[i]*options.height / max; j++) {
+                var x = i*series.length + s;
+                m[coordinate_hash(x,j)] = marker;
+            }
+        }
+    }
+    return m;
+}
+
+function drawMatrix(m, series, options) {
+    var width = _.flatten(series).length;
+    var nSeries = series.length;
+    var spacer = '';
+    if (width < options.width && series[0].length > 1) {
+        var spaceWidth = Math.round((options.width - width) / (series[0].length - 1));
+        spacer = Array(spaceWidth + 1).join(' ');
+    }
+
+    var rez = options.title ? options.title + '\n' : '';
+    for (var j=0; j<options.height; j++) {
+        for (var i=0; i<width; i++) {
+            var element = m[coordinate_hash(i,options.height - j - 1)];
+            rez += element ? colorElement(element) : ' ';
+            if (i > 0 && ((i % nSeries) === (nSeries - 1))) {
+                rez += spacer;
+            }
+        }
+        rez += '\n';
+    }
+    return rez;
+}
+
+function coordinate_hash(i,j) { 
+    return i + ',' + j; 
+}
+
+function colorElement(e) {
+    return e.color ? colors[e.color](e.icon) : e.icon;
+}
